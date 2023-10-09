28fad5bacffe3f50a2a89eeaee8eb6619b283a92
swagatika <j.swagatika2900@gmail.com>
2023-10-09 10:35:00 -0500

    1st commit

---
+++ src/test/java/com/inetBanking/testCases/BaseClass.java
@@ -0,0 +1,31 @@
+package com.inetBanking.testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseClass {
+
+	public String baseURL = "https://demo.guru99.com/v3/index.php";
+	public String userName = "mngr26593";
+	public String password = "1234567@";
+	public static WebDriver driver;
+	
+	@BeforeClass
+	 public WebDriver SetUp()
+	    {
+	        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//Drivers//chromedriver.exe");	       
+	        driver = new ChromeDriver();
+	        driver.manage().window().maximize();
+	        return driver;
+	    }
+	 
+	
+	@AfterClass
+	public void tearDown()
+	{	
+	driver.quit();
+	}
+	
+}
